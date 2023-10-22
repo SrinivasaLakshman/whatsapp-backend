a1e4bec002ceca4e154b745e79eb8e8faab0c24f
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2023-10-22 19:45:24 +0530

    setting port

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express()
+const port = process.env.PORT || 9000;
+
+// API routes
+app.get('/', (req,res) => res.status(200).send('hello world'));
+
+app.listen(port, () => console.log(`Listening on localhost:${port}`));
