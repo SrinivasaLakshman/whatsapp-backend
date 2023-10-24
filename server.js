f375794cab0342467ddfa4f3f6bf32428e35d07a
SrinivasaLakshman <srinivas.1718ccd@gmail.com>
2023-10-24 11:33:51 +0530

    triggering pusher

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ const pusher = new Pusher({
 // middleware
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+})
+
 // DB config
 const connection_url = 'mongodb+srv://admin:31123449@cluster0.cb49gax.mongodb.net/whatsappdb?retryWrites=true&w=majority'
 
@@ -38,7 +44,17 @@ db.once('open', () => {
 
     changeStream.on("change", (change) => {
         console.log(change);
-    })
+
+        if(change.operationType == 'insert') {
+            const messageDetails = change.fullDocument;
+            pusher.trigger('messages', 'inserted', {
+                    name: messageDetails.user,
+                    message: messageDetails.message,
+            });
+        } else {
+            console.log("Error triggering pusher");
+        }
+    });
 });
 
 // API routes
